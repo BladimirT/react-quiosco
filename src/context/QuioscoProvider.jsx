fa6ee5571c5689511b5486924cd4ed3bd5e4c877
BladimirT <bladimirtetzaguic@gmail.com>
2024-01-05 22:26:37 -0600

    Filtrando producto, para modal

--- src/context/QuioscoProvider.jsx
+++ src/context/QuioscoProvider.jsx
@@ -7,12 +7,32 @@ const QuioscoProvider = ({ children }) => {
 
     const [categorias, setCategorias] = useState(categoriasDB);
     const [categoriaActual, setCategoriaActual] = useState(categorias[0])
+    const [modal, setModal] = useState(false)
+    const [producto, setProducto] = useState(false)
+
+    const handleClickCategoria = id => {
+        const categoria = categorias.filter(categoria => categoria.id === id)[0]
+        setCategoriaActual(categoria)
+    }
+
+    const handleClickModal = () => {
+        setModal(!modal)
+    }
+
+    const handleSetProducto = producto => {
+        setProducto(producto)
+    }
 
     return (
         <QuioscoContext.Provider
             value={{
                 categorias,
-                categoriaActual
+                categoriaActual,
+                handleClickCategoria,
+                modal,
+                handleClickModal,
+                producto,
+                handleSetProducto
             }}
         >{children}</QuioscoContext.Provider>
     )
--- src/views/Inicio.jsx
+++ src/views/Inicio.jsx
@@ -1,10 +1,13 @@
 import Producto from '../components/Producto'
-import { productos } from '../data/productos'
+import { productos as data, productos } from '../data/productos'
 import useQuiosco from '../hooks/useQuiosco'
 
 export default function Inicio() {
 
-  const { categoriaActual} = useQuiosco()
+  const { categoriaActual } = useQuiosco()
+
+  const productos = data.filter(producto => producto.categoria_id === categoriaActual.id)
+
   return (
     <>
       <h1 className='text-4xl font-black'>{categoriaActual.nombre}</h1>
