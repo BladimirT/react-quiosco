90d40289a43c11b9bf83001ea382ca637d8c2853
BladimirT <bladimirtetzaguic@gmail.com>
2024-01-05 20:01:46 -0600

    Creacion del diseño del quiosco

---
+++ src/context/QuioscoProvider.jsx
@@ -0,0 +1,24 @@
+import { createContext, useState } from 'react'
+import { categorias as categoriasDB } from "../data/categorias"
+
+const QuioscoContext = createContext();
+
+const QuioscoProvider = ({ children }) => {
+
+    const [categorias, setCategorias] = useState(categoriasDB);
+    const [categoriaActual, setCategoriaActual] = useState(categorias[0])
+
+    return (
+        <QuioscoContext.Provider
+            value={{
+                categorias,
+                categoriaActual
+            }}
+        >{children}</QuioscoContext.Provider>
+    )
+}
+
+export {
+    QuioscoProvider
+}
+export default QuioscoContext
---
+++ src/views/Registro.jsx
@@ -0,0 +1,83 @@
+import { Link } from "react-router-dom"
+
+export default function Registro() {
+  return (
+    <>
+      <h1 className="text-4xl font-black">Crea tu Cuenta</h1>
+      <p>Crea tu cuenta llenando el formulario</p>
+
+      <div className="bg-white shadow-md rounded-md mt-10 px-5 py-10">
+        <form action="">
+
+          <div className="mb-4">
+            <label
+              className="text-slate-800"
+              htmlFor="name"
+            >Nombre</label>
+            <input
+              type="text"
+              id="name"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="name"
+              placeholder="Tu Nombre"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              className="text-slate-800"
+              htmlFor="email"
+            >Correo</label>
+            <input
+              type="email"
+              id="email"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="email"
+              placeholder="Tu Correo"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              className="text-slate-800"
+              htmlFor="password"
+            >Contraseña</label>
+            <input
+              type="password"
+              id="password"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="password"
+              placeholder="Tu Contraseña"
+            />
+          </div>
+
+          <div className="mb-4">
+            <label
+              className="text-slate-800"
+              htmlFor="password_confirmation"
+            >Repetir Contraseña</label>
+            <input
+              type="password"
+              id="password_confirmation"
+              className="mt-2 w-full p-3 bg-gray-50"
+              name="password_confirmation"
+              placeholder="Repetir Contraseña"
+            />
+          </div>
+
+          <input
+            type="submit"
+            value="Crear Cuenta"
+            className="bg-indigo-600 hover:bg-indigo-800 text-white w-full mt-5 p-3 uppercase font-bold cursor-pointer"
+          />
+        </form>
+      </div>
+
+      <nav className="mt-5">
+        <Link to="/auth/login">
+          ¿Ya tienes cuenta? Inicia Sesión
+        </Link>
+      </nav>
+    </>
+  )
+}
